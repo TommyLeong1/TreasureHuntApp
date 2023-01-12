f9f36a647647ddeeadb9f4361012861d7ad59a7a
LeongTommy1 <LeongTommy123@gmail.com>
2023-01-11 22:02:18 -0800

    add a treasure box image in home page

--- TreasureHuntApp/Homepage.swift
+++ TreasureHuntApp/Homepage.swift
@@ -30,12 +30,35 @@ class Homepage: UIViewController {
         return titleView
     }()
     
+    lazy var TreasureboxView: UIImageView = {
+        // Set the size of UIImageView.
+        let width: CGFloat = self.view.bounds.width/3
+        let height: CGFloat = 100
+        
+        // Set x, y of UIImageView.
+        let posX: CGFloat = (self.view.bounds.width - width)/2
+        let posY: CGFloat = (self.view.bounds.height - height)/2
+        
+        // Create UIImageView.
+        let TreasureboxView = UIImageView(frame: CGRect(x: posX, y: posY, width: width, height: height))
+        
+        // Create UIImage.
+        let TreasureboxImage: UIImage = UIImage(named: "Treasurebox")!
+        
+        // Set the image to UIImageView.
+        TreasureboxView.image = TreasureboxImage
+        
+        return TreasureboxView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Add titleViex View to view
         self.view.addSubview(self.titleView)
+        
+        // Add Treasurebox View to view
+        self.view.addSubview(self.TreasureboxView)
         
         // Change background color of view
         self.view.backgroundColor = UIColor.white
